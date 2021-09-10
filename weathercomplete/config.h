4c952b0323039d44ef9c4e661767222c07baa92d
Rij Dorfman <rdorfman419@outlook.com>
2021-09-10 12:12:55 -0700

    Arduino file with SDI12 dirty integration
    Used to test all hardware on WeatherChimes prototype, no Internet connectivity

---
+++ weathercomplete/config.h
@@ -0,0 +1,22 @@
+"{\
+  'general':\
+  {\
+    'name':'Device',\   
+    'instance':1,\
+    'interval':2000\
+  },\
+  'components':[\
+    {\
+      'name':'TSL2591',\
+      'params':'default'\
+    },\
+    {\
+      'name':'SD',\
+      'params':[true,1000,10,'test',true]\
+    },\
+    {\
+      'name':'SHT31D',\
+      'params':'default'\
+    }\
+  ]\
+}"
