c13480c17daaffc90da09ccbebac9636a129f363
Will Richards <richawil@oregonstate.edu>
2022-05-16 14:58:35 -0700

    Update arduino_secrets.h

--- WeatherChimes/arduino_secrets.h
+++ WeatherChimes/arduino_secrets.h
@@ -1,11 +1,11 @@
 
 // Wifi settings
-#define SECRET_SSID "OSU_Access" // WiFi Name
+#define SECRET_SSID "" // WiFi Name
 #define SECRET_PASS ""  // WiFi Password
 
 // MQTT Settings
-#define BROKER_USER "User"
-#define BROKER_PASSWORD "password"
-#define SECRET_BROKER "cas-mosquitto.biossys.oregonstate.edu"
+#define BROKER_USER ""
+#define BROKER_PASSWORD ""
+#define SECRET_BROKER ""
 #define BROKER_PORT 1883
 #define SITE_NAME "WeatherChimes" // The name of the location where these nodes will be placed
