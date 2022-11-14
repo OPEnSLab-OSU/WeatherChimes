d00aa568b7993d016da061e3ad6f176d11f1d6c0
Will Richards <richawil@oregonstate.edu>
2022-11-14 11:49:16 -0800

    New code running on an old device

---
+++ WeatherChimes/WeatherChimes.ino
@@ -0,0 +1,84 @@
+/**
+ * In lab use case example for the WeatherChimes project
+ * 
+ * This project uses SDI12, TSL2591 and an SHT31 sensor to log environment data and logs it to both the SD card and also MQTT/MongoDB
+ * 
+ * MANAGER MUST BE INCLUDED FIRST IN ALL CODE
+ */
+
+#include <Loom_Manager.h>
+
+#include <Hardware/Loom_Hypnos/Loom_Hypnos.h>
+
+#include <Sensors/Loom_Analog/Loom_Analog.h>
+#include <Sensors/SDI12/Loom_SDI12/Loom_SDI12.h>
+#include <Sensors/I2C/Loom_SHT31/Loom_SHT31.h>
+#include <Sensors/I2C/Loom_TSL2591/Loom_TSL2591.h>
+
+#include <Internet/Connectivity/Loom_Wifi/Loom_Wifi.h>
+#include <Internet/Logging/Loom_MQTT/Loom_MQTT.h>
+
+Manager manager("Chime", 5);
+
+// Analog for reading battery voltage
+Loom_Analog analog(manager);
+
+// Create a new Hypnos object
+Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_2, TIME_ZONE::PST);
+
+// Create the TSL2591 and SHT classes
+Loom_SHT31 sht(manager);
+Loom_TSL2591 tsl(manager);
+Loom_SDI12 sdi(manager, 11);
+
+Loom_WIFI wifi(manager);
+Loom_MQTT mqtt(manager, wifi.getClient());
+
+// Called when the interrupt is triggered 
+void isrTrigger(){
+  hypnos.wakeup();
+}
+
+void setup() {
+
+  // Wait 20 seconds for the serial console to open
+  manager.beginSerial();
+
+  // Enable the hypnos rails
+  hypnos.enable();
+
+  // Load the WiFi login credentials from a file on the SD card
+  wifi.loadConfigFromJSON(hypnos.readFile("wifi_creds.json"));
+  mqtt.loadConfigFromJSON(hypnos.readFile("mqtt_creds.json"));
+
+  // Initialize all in-use modules
+  manager.initialize();
+
+  // Register the ISR and attach to the interrupt
+  hypnos.registerInterrupt(isrTrigger);
+}
+
+void loop() {
+
+  // Set the RTC interrupt to trigger 10 seconds from when the device woke up
+  hypnos.setInterruptDuration(TimeSpan(0, 0, 10, 0));
+
+  // Measure and package the data
+  manager.measure();
+  manager.package();
+  
+  // Print the current JSON packet
+  manager.display_data();            
+
+  // Log the data to the SD card              
+  hypnos.logToSD();
+
+  // Publish the collected data to MQTT
+  mqtt.publish();
+
+  // Reattach to the interrupt after we have set the alarm so we can have repeat triggers
+  hypnos.reattachRTCInterrupt();
+  
+  // Put the device into a deep sleep, operation HALTS here until the interrupt is triggered
+  hypnos.sleep();
+}
